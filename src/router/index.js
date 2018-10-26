a3da9637cb662737e5b82c515e1bb8badf7d3413
Jesray Garciano <jesray.garciano@gmail.com>
2018-10-26 13:29:44 +0800

    Added signup route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
+import SignUp from '@/components/Admin/SignUp'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
